21f34cfd2e9d86dfd88b2c053cf78f4094cc59c8
srdshukla4 <srdshukla4@gmail.com>
2022-02-08 23:55:29 +0530

    commit for 80 feb 2022

---
+++ hello_world.js
@@ -0,0 +1,13 @@
+//initialize
+var var_a = 10;
+let let_a = 10;
+
+console.log(var_a);
+console.log(let_a);
+
+//reinitialize
+var var_a = `ten`;
+let let_a = `ten`;
+
+console.log(var_a);
+console.log(let_a);
